a2880a25276446d7dbd03534f22e3e90182cb861
beebikun <beebikun@gmail.com>
2014-08-09 22:10:59 +1000

    Убрано максимальное количество слайдов в 5шт, теперь ширина проставляется автоматически; переделана структура нижней навигации

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ var paralaxSlider =function(element) {
 }
 
 paralaxSlider.prototype._init = function(element) {
-    this.body = element;
-    this.content = document.querySelector('.sp-content')
-    this.slider = this.content.querySelector('.sp-slider')
-    this.images = this.slider.querySelectorAll('li')
-    this.bg = this.content.querySelector('.sp-parallax-bg')
+    this.settings = {
+        arrowLeft: 'fa-chevron-circle-left',
+        arrowRight: 'fa-chevron-circle-right',
+        paralaxStep: 25,
+    }
+    this.element = element
+    this.body = element.querySelector('.sp-content-body');
+    this.content = element.querySelector('.sp-content');
+    this.slider = this.content.querySelector('.sp-slider');
+    this.images = this.slider.querySelectorAll('li[data-nav="none"]');
+    this.bg = this.content.querySelector('.sp-parallax-bg');
     this._init_navs()
     this._init_arrow()
 };
@@ -15,48 +21,36 @@ paralaxSlider.prototype._init = function(element) {
 paralaxSlider.prototype._init_navs = function(first_argument) {
     this.navs = new Array;
     var count = this.images.length;
-    var half = Math.floor(count/2)
-    var step = 18;
+    var imagesWidth =  100/count + '%';
     var self = this;
-    for (var i = count - 1; i >= 0; i--) {
-        var margin = (count % 2) ? (i != half) ? step*(i - half) : 0
-                                 : (step*(i-half) + step*0.5)
-        var input = document.createElement('input');
+    this.slider.style.width = count * 100 + '%';
+    for (var i = 0; i < count; i++) {
         var label = document.createElement('label');
-        var id = 'sp-slider-button-' + i;
-        input.classname = 'sp-selector'
-        input.setAttribute('id', id)
-        input.setAttribute('type', 'radio')
-        input.setAttribute('name', 'sp-slider-radio-set')
-        input.style.marginLeft = margin + 'px';
-        input.i = i
-        input.onclick = function(){self.slideTo(true, this.i)}
-        this.navs[i] = input
-
-        label.setAttribute('for', id)
-        label.classname = 'button-label'
-        label.style.marginLeft = margin + 'px';
-
-        self.body.insertBefore(input, self.content)
-        self.body.insertBefore(label, self.content)
+        label.onclick = function(){self.slideTo(true, this.i)}
+        label.className = 'sp-button-label'
+        self.body.appendChild(label)
+        this.navs[i] = label
+        this.images[i].style.width = imagesWidth;
+        this.images[i].setAttribute("data-nav", "create")
     };
-    this.slideTo(true);
+    this.slideTo()
 };
 
 paralaxSlider.prototype._init_arrow = function() {
     var self = this;
 
-    function create(classname, isNext){
+    function create(classname, isNext, arrowClass){
         var arrow = document.createElement( 'label' );
         arrow.className = 'sp-arrow ' + classname;
-        self.body.insertBefore(arrow, self.content)
+        arrow.innerHTML = '<i class="fa fa-3x ' + self.settings[arrowClass] + '"></i>'
+        self.element.insertBefore(arrow, self.body)
         arrow.onclick = function(){
             self.slideTo(isNext)
         }
     }
 
-    create('sp-arrow-left', false)
-    create('sp-arrow-right', true)
+    create('sp-arrow-left', false, 'arrowLeft')
+    create('sp-arrow-right', true, 'arrowRight')
 };
 
 
@@ -82,7 +76,7 @@ paralaxSlider.prototype.slideTo = function(isNext, indx) {
     }
     if(indx === undefined){
         if(this.active){
-            this.active.removeAttribute('checked')
+            this.active.removeAttribute('data-active')
             var indx = getNextIndx();
         } else{
             var indx = 0
@@ -92,8 +86,8 @@ paralaxSlider.prototype.slideTo = function(isNext, indx) {
         this.images[i].style.opacity = 0.4
     };
     this.slider.style.left = -100 * indx + '%'
-    this.bg.style.backgroundPositionX = -100*indx + 'px'
+    this.bg.style.backgroundPositionX = this.settings.paralaxStep*-1*indx + 'px'
     this.active = this.navs[indx]
-    this.active.setAttribute('checked', 'checked')
+    this.active.setAttribute('data-active', 'active')
     this.images[indx].style.opacity = 1
 };
